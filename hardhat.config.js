977b41aa5d78f57b2585ec5f55e93137ebbd88d2
Rhythm Nayyer <rhythmnayyer@Rhythms-MacBook-Air-2.local>
2023-07-28 08:25:33 +0530

    Hardhat config changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,36 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-
-// task("accounts", "Print the list of account",async()=>{
-//   const accounts=await ethers.getSigners();
-
-//   for( const account of accounts){
-//     console.log(account.address);
-//   }
-// });
-
-
-// Go to https://infura.io, sign up, create a new API key
-// in its dashboard, and replace "KEY" with it
-
-
-// Replace this private key with your Sepolia account private key
-// To export your private key from Coinbase Wallet, go to
-// Settings > Developer Settings > Show private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Beware: NEVER put real Ether into testing accounts
 require('dotenv').config();
-//const {INFURA_API_KEY, API_URL}= process.env;
 const API_URL=process.env.API_URL;
 const SEPOLIA_PRIVATE_KEY=process.env.SEPOLIA_PRIVATE_KEY;
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: [API_URL],
+      url: API_URL,
       accounts: [SEPOLIA_PRIVATE_KEY]
     }
   }
